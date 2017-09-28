a2e0cac25c691c2ee307b4227ea2164e2f4a6a82
Brad Brown <brad.brown@rackspace.com>
2017-09-28 08:41:05 -0500

    Enhancement: refactor locustfile import

--- locust/main.py
+++ locust/main.py
@@ -1,3 +1,4 @@
+from importlib.machinery import SourceFileLoader
 import inspect
 import logging
 import os
@@ -331,32 +332,8 @@ def load_locustfile(path):
     """
     # Get directory and locustfile name
     directory, locustfile = os.path.split(path)
-    # If the directory isn't in the PYTHONPATH, add it so our import will work
-    added_to_path = False
-    index = None
-    if directory not in sys.path:
-        sys.path.insert(0, directory)
-        added_to_path = True
-    # If the directory IS in the PYTHONPATH, move it to the front temporarily,
-    # otherwise other locustfiles -- like Locusts's own -- may scoop the intended
-    # one.
-    else:
-        i = sys.path.index(directory)
-        if i != 0:
-            # Store index for later restoration
-            index = i
-            # Add to front, then remove from original position
-            sys.path.insert(0, directory)
-            del sys.path[i + 1]
     # Perform the import (trimming off the .py)
-    imported = __import__(os.path.splitext(locustfile)[0])
-    # Remove directory from path if we added it ourselves (just to be neat)
-    if added_to_path:
-        del sys.path[0]
-    # Put back in original index if we moved it
-    if index is not None:
-        sys.path.insert(index + 1, directory)
-        del sys.path[0]
+    imported = SourceFileLoader(os.path.splitext(locustfile)[0], path).load_module()
     # Return our two-tuple
     locusts = dict(filter(is_locust, vars(imported).items()))
     return imported.__doc__, locusts
